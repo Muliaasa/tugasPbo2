b29f7ae6785ece8dd7a6df35b55a24de057cbbe8
Muliaasa <muliaasa29@gmail.com>
2024-08-27 16:07:58 +0700

    Add files via upload

---
+++ MatematikaCanggih.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Asus X452C
+ */
+public class MatematikaCanggih {
+    public double tambah(double a, double b, double c){
+        return a + b + c;
+    }
+
+    int tambah(int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    double tambah(double d, double d0) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
